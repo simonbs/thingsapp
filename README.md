6f3e681e2f8c5cc29867312d3913b5a6b222018a
Simon Støvring <mail@simonbs.dk>
2017-01-28 20:13:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# thingsapp
+
+Node module for creating todos in Things.app by Cultured Code.
+
+## Installation
+
+Install using `npm install --save git+ssh://git@github.com/simonbs/things.git`.
+
+## Usage
+
+When creating an instance of Things, you should pass your account ID and start index. Find these by inspecting HTTP traffic to `https://cloud.culturedcode.com` using [Charles](https://www.charlesproxy.com) or similar.
+
+The UUID in the requests is your account ID. Consider the following call to the Things Cloud API.
+
+```
+https://cloud.culturedcode.com/version/1/history/THIS-IS-YOUR-ACCOUNT-ID/items?start-index=5612
+```
+
+In above request, `THIS-IS-YOUR-ACCOUNT-ID` is your account ID and your start index is 5612. 
+
+When you have acquired both your account ID and your start index you can use the thingsapp module as shown below.
+
+```
+var todo = {
+  title: 'Pack for vacation',
+  note: 'GoPro, selfiestick, MacBook charger and tickets'
+}
+var things = new Things('THIS-IS-YOUR-ACCOUNT-ID', 5612)
+things.createTodo(todo, function(err) {
+  // Handle errors.
+})
+```
+
+In your todo you can set the `where` key to either `inbox` or `today` to specify the location of the todo. Default is the inbox.
---
+++ index.js
@@ -0,0 +1,104 @@
+var uuid4 = require('uuid/v4')
+var moment = require('moment')
+var request = require('request')
+
+function Things(accountId, startIndex) { 
+  this.accountId = accountId
+  this.startIndex = startIndex
+}
+
+Things.prototype.createTodo = function(todo, callback) {
+  var things = this
+  this.getCurrentIndex(this.startIndex, function(err, idx) {
+    if (err) { return callback(err) }
+    things.constructTodoItem(todo, function(err, todoItem) {
+      var url = 'https://cloud.culturedcode.com/version/1/history/' + things.accountId + '/items'
+      if (err) { return callback(err) }
+      request.post({
+        url: url,
+        json: {
+          'current-item-index': idx,
+          'items': [ todoItem ],
+          'schema': 1
+        },
+        headers: {
+          'User-Agent': 'ThingsMac/20808500mas (x86_64; OS X 10.12.2; en_DK)',
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Content-Encoding': 'UTF-8'
+        }
+      }, callback)
+    })
+  })
+}
+
+Things.prototype.constructTodoItem = function(todo, callback) {
+  title = todo['title'] || 'New todo'
+  duedate = todo['duedate'] || null
+  note = todo['note'] || null
+  destination = todo['where'] || 'inbox'
+  uid = uuid4().toUpperCase()
+  now = Date.now() / 1000
+  if (destination == 'today') {
+    st = 2
+  } else if (destination == 'inbox') {
+    st = 0
+  } else {
+    return callback('Unsupported destination \'' + destination + '\'.')
+  }
+  if (st == 2) {
+    sr = moment(moment(new Date()).format('YYYY-MM-DD')).unix()
+  } else {
+    sr = null
+  }
+  if (note != null) {
+    note = '<note xml:space=\"preserve\">' + note + '</note>'
+  }
+  item = {}
+  item[uid] = {
+    "t": 0,
+    "e": "Task2",
+    "p": {
+      "acrd": null,
+      "ar": [],
+      "cd": now,
+      "dd": duedate,
+      "dl": [],
+      "do": 0,
+      "icc": 0,
+      "icp": false,
+      "icsd": null,
+      "ix": 0,
+      "md": now,
+      "nt": note,
+      "pr": [],
+      "rr": null,
+      "rt": [],
+      "sp": null,
+      "sr": sr,
+      "ss": 0,
+      "st": st,
+      "tg": [],
+      "ti": 0,
+      "tp": 0,
+      "tr": false,
+      "tt": title
+    }
+  }
+  callback(null, item)
+}
+
+Things.prototype.getCurrentIndex = function(startIndex, callback) {
+  url = 'https://thingscloud.appspot.com/version/1/history/'
+    + this.accountId + '/items?start-index=' + startIndex
+  request.get({
+    url: url,
+  }, 
+  function(err, httpResponse, body) {
+    if (err) { return callback(err) }
+    var json = JSON.parse(body)
+    callback(null, json['current-item-index'])
+  })
+}
+
+module.exports = Things
+
